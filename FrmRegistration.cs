921d546eeb8783ba90757738a11e97b64e53fd7b
mulano123 <joshuamulano26@gmail.com>
2023-11-11 22:19:29 +0800

    final

--- FrmRegistration.cs
+++ FrmRegistration.cs
@@ -17,47 +17,10 @@ namespace CreatingTextFile
         public FrmRegistration()
         {
             InitializeComponent();
-      
+            listOfProgram();
         }
 
-        private void btnRegister_Click(object sender, EventArgs e)
-        {
-            string getStudentNo = txtStudentNo.Text;
-            string getFirstName = txtFirstName.Text;
-            string getMiddleName = txtMiddleInitial.Text;
-            string getLastName = txtLastName.Text;
-            string getAge = txtAge.Text;
-            string getBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
-            string getProgram = cmbProgram.Text;
-            string getGender = cmbGender.Text;
-            string getContact = txtContactNo.Text;
-            string setFileName = String.Concat(getStudentNo, ".txt");
-
-            string docPath2 = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            StreamWriter outputFile = new StreamWriter(Path.Combine(docPath2, setFileName));
-
-            string[] info = {
-                "Student No: " + getStudentNo,                           
-                "FullName: " + getLastName + ", " + getFirstName + ", " + getMiddleName,                            
-                "Program: " + getProgram,                             
-                "Gender: " + getGender,                            
-                "Age: " + getAge,                            
-                "Birthday: " + getBirthday,                            
-                "Contact No: " + getContact};
-
-
-            Console.WriteLine(getStudentNo);
-            foreach (string i in info)
-            {
-                outputFile.WriteLine(i);
-            }
-            outputFile.Close();
-            MessageBox.Show("Successfully saved!!!");
-            Close();
-
-        }
-
-        private void FrmRegistration_Load(object sender, EventArgs e)
+        public void listOfProgram()
         {
             string[] ListOfPrograms = new string[] {
                 "BS in Information Technology",
